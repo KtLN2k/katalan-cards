cf9aa2f4d11ba1d58addb51922cafb323c95eebb
Ben Katalan <98488242+KtLN2k@users.noreply.github.com>
2025-06-09 13:36:29 +0300

    Update Register.page.tsx
    Update user after successful signup.

--- src/pages/Register/Register.page.tsx
+++ src/pages/Register/Register.page.tsx
@@ -3,6 +3,9 @@ import { useForm } from "react-hook-form";
 import { joiResolver } from "@hookform/resolvers/joi";
 import { registerSchema } from "../../validations/register.joi";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
 
 export type FormData = {
   name: {
@@ -29,6 +32,8 @@ export type FormData = {
 };
 
 export default function RegisterForm() {
+  const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
@@ -58,213 +63,228 @@ export default function RegisterForm() {
     try {
       await axios.post(
         "https://monkfish-app-z9uza.ondigitalocean.app/bcard2/users",
-        data,
+        data
       );
-      console.log("Success!");
+      toast.success("ההרשמה בוצעה בהצלחה 🎉", { autoClose: 2000 });
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
     } catch (err) {
       console.error("Error submitting form:", err);
+      toast.error("שגיאה בהרשמה ❌");
     }
   };
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-100 px-4 py-12 dark:bg-gray-900">
-      <div className="w-full max-w-4xl rounded-xl bg-white p-10 shadow-xl dark:bg-gray-800">
-        <h2 className="mb-10 text-center text-3xl font-bold text-gray-800 dark:text-white">
-          Create an Account
-        </h2>
-        <form
-          onSubmit={handleSubmit(submitForm)}
-          className="grid grid-cols-1 gap-6 md:grid-cols-3"
-        >
-          {/* Section 1: Personal Info */}
-          <div className="flex flex-col gap-4">
-            <h3 className="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-200">
-              Personal Info
-            </h3>
-            <FloatingLabel
-              variant={"filled"}
-              {...register("name.first")}
-              label="First Name"
-              type="text"
-            />
-            {errors.name?.first && (
-              <p className="text-sm text-red-500">
-                {errors.name.first.message}
-              </p>
-            )}
+    <>
+      <main className="flex min-h-screen items-center justify-center bg-gray-100 px-4 py-12 dark:bg-gray-900">
+        <div className="w-full max-w-4xl rounded-xl bg-white p-10 shadow-xl dark:bg-gray-800">
+          <h2 className="mb-10 text-center text-3xl font-bold text-gray-800 dark:text-white">
+            Create an Account
+          </h2>
+          <form
+            onSubmit={handleSubmit(submitForm)}
+            className="grid grid-cols-1 gap-6 md:grid-cols-3"
+          >
+            {/* Section 1: Personal Info */}
+            <div className="flex flex-col gap-4">
+              <h3 className="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-200">
+                Personal Info
+              </h3>
+              <FloatingLabel
+                variant={"filled"}
+                {...register("name.first")}
+                label="First Name"
+                type="text"
+              />
+              {errors.name?.first && (
+                <p className="text-sm text-red-500">
+                  {errors.name.first.message}
+                </p>
+              )}
 
-            <FloatingLabel
-              variant={"filled"}
-              {...register("name.middle")}
-              label="Middle Name"
-              type="text"
-            />
-            {errors.name?.middle && (
-              <p className="text-sm text-red-500">
-                {errors.name.middle.message}
-              </p>
-            )}
+              <FloatingLabel
+                variant={"filled"}
+                {...register("name.middle")}
+                label="Middle Name"
+                type="text"
+              />
+              {errors.name?.middle && (
+                <p className="text-sm text-red-500">
+                  {errors.name.middle.message}
+                </p>
+              )}
 
-            <FloatingLabel
-              variant={"filled"}
-              {...register("name.last")}
-              label="Last Name"
-              type="text"
-            />
-            {errors.name?.last && (
-              <p className="text-sm text-red-500">{errors.name.last.message}</p>
-            )}
+              <FloatingLabel
+                variant={"filled"}
+                {...register("name.last")}
+                label="Last Name"
+                type="text"
+              />
+              {errors.name?.last && (
+                <p className="text-sm text-red-500">
+                  {errors.name.last.message}
+                </p>
+              )}
 
-            <FloatingLabel
-              variant={"filled"}
-              {...register("phone")}
-              label="Phone"
-              type="tel"
-            />
-            {errors.phone && (
-              <p className="text-sm text-red-500">{errors.phone.message}</p>
-            )}
-          </div>
+              <FloatingLabel
+                variant={"filled"}
+                {...register("phone")}
+                label="Phone"
+                type="tel"
+              />
+              {errors.phone && (
+                <p className="text-sm text-red-500">{errors.phone.message}</p>
+              )}
+            </div>
 
-          {/* Section 2: Account & Image */}
-          <div className="flex flex-col gap-4">
-            <h3 className="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-200">
-              Account & Image
-            </h3>
-            <FloatingLabel
-              variant={"filled"}
-              {...register("email")}
-              label="Email"
-              type="email"
-            />
-            {errors.email && (
-              <p className="text-sm text-red-500">{errors.email.message}</p>
-            )}
+            {/* Section 2: Account & Image */}
+            <div className="flex flex-col gap-4">
+              <h3 className="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-200">
+                Account & Image
+              </h3>
+              <FloatingLabel
+                variant={"filled"}
+                {...register("email")}
+                label="Email"
+                type="email"
+              />
+              {errors.email && (
+                <p className="text-sm text-red-500">{errors.email.message}</p>
+              )}
 
-            <FloatingLabel
-              variant={"filled"}
-              {...register("password")}
-              label="Password"
-              type="password"
-            />
-            {errors.password && (
-              <p className="text-sm text-red-500">{errors.password.message}</p>
-            )}
+              <FloatingLabel
+                variant={"filled"}
+                {...register("password")}
+                label="Password"
+                type="password"
+              />
+              {errors.password && (
+                <p className="text-sm text-red-500">
+                  {errors.password.message}
+                </p>
+              )}
 
-            <FloatingLabel
-              variant={"filled"}
-              {...register("image.url")}
-              label="Image URL"
-              type="url"
-            />
-            {errors.image?.url && (
-              <p className="text-sm text-red-500">{errors.image.url.message}</p>
-            )}
+              <FloatingLabel
+                variant={"filled"}
+                {...register("image.url")}
+                label="Image URL"
+                type="url"
+              />
+              {errors.image?.url && (
+                <p className="text-sm text-red-500">
+                  {errors.image.url.message}
+                </p>
+              )}
 
-            <FloatingLabel
-              variant={"filled"}
-              {...register("image.alt")}
-              label="Image Alt Text"
-              type="text"
-            />
-            {errors.image?.alt && (
-              <p className="text-sm text-red-500">{errors.image.alt.message}</p>
-            )}
+              <FloatingLabel
+                variant={"filled"}
+                {...register("image.alt")}
+                label="Image Alt Text"
+                type="text"
+              />
+              {errors.image?.alt && (
+                <p className="text-sm text-red-500">
+                  {errors.image.alt.message}
+                </p>
+              )}
 
-            <label className="mt-2 flex items-center gap-2">
-              <input type="checkbox" {...register("isBusiness")} />
-              <span className="text-sm text-gray-700 dark:text-gray-300">
-                Business Account
-              </span>
-            </label>
-          </div>
+              <label className="mt-2 flex items-center gap-2">
+                <input type="checkbox" {...register("isBusiness")} />
+                <span className="text-sm text-gray-700 dark:text-gray-300">
+                  Business Account
+                </span>
+              </label>
+            </div>
 
-          {/* Section 3: Address */}
-          <div className="flex flex-col gap-4">
-            <h3 className="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-200">
-              Address
-            </h3>
-            <FloatingLabel
-              variant={"filled"}
-              {...register("address.state")}
-              label="State"
-              type="text"
-            />
-            {errors.address?.state && (
-              <p className="text-sm text-red-500">
-                {errors.address.state.message}
-              </p>
-            )}
+            {/* Section 3: Address */}
+            <div className="flex flex-col gap-4">
+              <h3 className="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-200">
+                Address
+              </h3>
+              <FloatingLabel
+                variant={"filled"}
+                {...register("address.state")}
+                label="State"
+                type="text"
+              />
+              {errors.address?.state && (
+                <p className="text-sm text-red-500">
+                  {errors.address.state.message}
+                </p>
+              )}
 
-            <FloatingLabel
-              variant={"filled"}
-              {...register("address.country")}
-              label="Country"
-              type="text"
-            />
-            {errors.address?.country && (
-              <p className="text-sm text-red-500">
-                {errors.address.country.message}
-              </p>
-            )}
+              <FloatingLabel
+                variant={"filled"}
+                {...register("address.country")}
+                label="Country"
+                type="text"
+              />
+              {errors.address?.country && (
+                <p className="text-sm text-red-500">
+                  {errors.address.country.message}
+                </p>
+              )}
 
-            <FloatingLabel
-              variant={"filled"}
-              {...register("address.city")}
-              label="City"
-              type="text"
-            />
-            {errors.address?.city && (
-              <p className="text-sm text-red-500">
-                {errors.address.city.message}
-              </p>
-            )}
+              <FloatingLabel
+                variant={"filled"}
+                {...register("address.city")}
+                label="City"
+                type="text"
+              />
+              {errors.address?.city && (
+                <p className="text-sm text-red-500">
+                  {errors.address.city.message}
+                </p>
+              )}
 
-            <FloatingLabel
-              variant={"filled"}
-              {...register("address.street")}
-              label="Street"
-              type="text"
-            />
-            {errors.address?.street && (
-              <p className="text-sm text-red-500">
-                {errors.address.street.message}
-              </p>
-            )}
+              <FloatingLabel
+                variant={"filled"}
+                {...register("address.street")}
+                label="Street"
+                type="text"
+              />
+              {errors.address?.street && (
+                <p className="text-sm text-red-500">
+                  {errors.address.street.message}
+                </p>
+              )}
 
-            <FloatingLabel
-              variant={"filled"}
-              {...register("address.houseNumber")}
-              label="House Number"
-              type="number"
-            />
-            {errors.address?.houseNumber && (
-              <p className="text-sm text-red-500">
-                {errors.address.houseNumber.message}
-              </p>
-            )}
+              <FloatingLabel
+                variant={"filled"}
+                {...register("address.houseNumber")}
+                label="House Number"
+                type="number"
+              />
+              {errors.address?.houseNumber && (
+                <p className="text-sm text-red-500">
+                  {errors.address.houseNumber.message}
+                </p>
+              )}
 
-            <FloatingLabel
-              variant={"filled"}
-              {...register("address.zip")}
-              label="Zip Code"
-              type="number"
-            />
-            {errors.address?.zip && (
-              <p className="text-sm text-red-500">
-                {errors.address.zip.message}
-              </p>
-            )}
-          </div>
+              <FloatingLabel
+                variant={"filled"}
+                {...register("address.zip")}
+                label="Zip Code"
+                type="number"
+              />
+              {errors.address?.zip && (
+                <p className="text-sm text-red-500">
+                  {errors.address.zip.message}
+                </p>
+              )}
+            </div>
 
-          {/* Submit Button (spans all columns) */}
-          <div className="md:col-span-3">
-            <Button type="submit" className="mt-4 w-full" color="purple">
-              Register
-            </Button>
-          </div>
-        </form>
-      </div>
-    </main>
+            {/* Submit Button (spans all columns) */}
+            <div className="md:col-span-3">
+              <Button type="submit" className="mt-4 w-full" color="purple">
+                Register
+              </Button>
+            </div>
+          </form>
+        </div>
+      </main>
+      <ToastContainer />
+    </>
   );
 }
